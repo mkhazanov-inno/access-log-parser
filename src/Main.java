505d1e1124c30aca470c1bb68a8c4c70046bc504
Maxim <mkhazanov@inno.tech>
2025-09-01 22:38:09 +0300

    Added file path verifier

--- src/Main.java
+++ src/Main.java
@@ -1,9 +1,35 @@
+import java.io.File;
 import java.util.Scanner;
 
 public class Main {
+
     public static void main(String[] args) {
-        System.out.println("Введите текст и нажмите <Enter>: ");
-        String text = new Scanner(System.in).nextLine();
-        System.out.println("Длина текста: " + text.length());
+        Scanner sc = new Scanner(System.in);
+        String path;
+        boolean fileExists;
+        boolean isDirectory;
+        int counter = 0;
+
+
+        while (true) {
+            System.out.println("Введите путь к файлу:");
+            path = sc.nextLine();
+            File file = new File(path);
+            fileExists = file.exists();
+            isDirectory = file.isDirectory();
+            if (!fileExists){
+                System.out.println("Введенная строка не является путем к файлу: объект не существует");
+                continue;
+            } else if (isDirectory) {
+                System.out.println("Введенная строка не является путем к файлу: это каталог");
+                continue;
+            }
+            counter++;
+            System.out.println("Путь указан верно");
+            System.out.println("Это файл номер " + counter);
+
+
+        }
+
     }
 }
